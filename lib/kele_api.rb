b18ae56d8bd9056efb6627ce39a6fba3d9ecab14
Kevin Lattner <analogy48@yahoo.com>
2017-11-21 20:48:39 +0000

    Checkpoint Submission

--- lib/kele_api.rb
+++ lib/kele_api.rb
@@ -52,6 +52,19 @@ class KeleApi
     JSON.parse(response.body)
   end
   
+  def create_submission(checkpoint_id, assignment_branch, assignment_commit_link, comment, enrollment_id)
+    response = self.class.post(base_api_endpoint("checkpoint_submissions"), 
+    body:{ 
+      "checkpoint_id": checkpoint_id, 
+      "assignment_branch": assignment_branch, 
+      "assignment_commit_link": assignment_commit_link, 
+      "comment": comment,
+      "enrollment_id": enrollment_id
+    },
+    headers: { "authorization" => @auth_token })
+    JSON.parse(response.body)
+  end
+  
   
   private
 
